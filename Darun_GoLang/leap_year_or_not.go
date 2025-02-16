4def90bc128c885d8968d72c546dcd7ddf54ddc1
Darun S <149518721+sdarun7@users.noreply.github.com>
2025-02-16 15:41:41 +0530

    Create leap_year_or_not.go

---
+++ Darun_GoLang/leap_year_or_not.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+    var year int
+    fmt.Print("Enter a year: ")
+    fmt.Scan(&year)
+
+    if (year%4 == 0 && year%100 != 0) || (year%400 == 0) {
+        fmt.Println(year,"is a Leap Year")
+    } else {
+        fmt.Println(year,"is NOT a Leap Year")
+    }
+}

d7b4808c11c66ab908ace977cfd4b3fd882c4087
Siddharthb04 <137971611+Siddharthb04@users.noreply.github.com>
2025-02-16 20:24:58 +0530

    Create gcd.go

---
+++ Siddharth_golang/gcd.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+func main() {
+	var a, b int
+	fmt.Print("Enter two numbers: ")
+	fmt.Scan(&a, &b)
+	fmt.Println("GCD:", gcd(a, b))
+}
